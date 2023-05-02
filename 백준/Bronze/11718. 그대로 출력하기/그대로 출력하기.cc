cdc1497f3b5ccb0f7399fa568b33f1f5a9ba3721
kyucho1009 <126551132+kyucho1009@users.noreply.github.com>
2023-05-03 01:08:07 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+
+    while (1)
+    {
+        getline(cin, str);
+        if (str == "")
+            break;
+        cout << str << "\n";
+    }
+    return 0;
+}
