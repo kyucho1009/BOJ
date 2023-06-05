c836ebea30948737e002bfabe857ad1f8279a57f
kyucho1009 <126551132+kyucho1009@users.noreply.github.com>
2023-06-06 03:11:18 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "         ,r\'\"7";
+    cout << "\n";
+    cout << "r`-_   ,'  ,/";
+    cout << "\n";
+    cout << " \\. \". L_r'";
+    cout << "\n";
+    cout << "   `~\\/";
+    cout << "\n";
+    cout << "      |";
+    cout << "\n";
+    cout << "      |";
+}
