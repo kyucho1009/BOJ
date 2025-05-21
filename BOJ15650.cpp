f45b1f63e35caff836cc884c285760aa197cbaf2
kyucho1009 <chocastle4@gmail.com>
2025-05-21 15:41:37 +0900

    BOJ15650

---
+++ BOJ15650.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int M;
+int arr[10];
+bool isused[10];
+
+void func(int num)
+{
+    if (num == M)
+    {
+        for (int i = 0; i < M; i++)
+            cout << arr[i] << " ";
+        cout << "\n";
+        return;
+    }
+    for (int i = 1; i <= N; i++)
+    {
+        // 사용된 숫자인가, 이전보다 큰 숫자인가
+        if (num == 0)
+        {
+            if (!isused[i])
+            {
+                arr[num] = i;
+                isused[i] = 1;
+                func(num + 1);
+                isused[i] = 0;
+            }
+        }
+        else
+        {
+            if (!isused[i] && arr[num - 1] < i)
+            {
+                arr[num] = i;
+                isused[i] = 1;
+                func(num + 1);
+                isused[i] = 0;
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < 10; i++)
+        arr[i] = 10;
+    func(0);
+    return 0;
+}
