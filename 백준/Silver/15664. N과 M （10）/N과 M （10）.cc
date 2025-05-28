32758fd879eeb2435e37da871d91d859bda49731
kyucho1009 <126551132+kyucho1009@users.noreply.github.com>
2025-05-28 21:56:46 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int M;
+int arr[10];
+int digit[10];
+bool isused[10];
+
+void func(int num)
+{
+    if (num == M)
+    {
+        for (int i = 0; i < M; i++)
+            cout << arr[i] << " ";
+        cout << "\n";
+        return;
+    }
+    int tmp = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (!isused[i] && digit[i] != tmp && arr[num - 1] <= digit[i])
+        {
+            arr[num] = digit[i];
+            isused[i] = 1;
+            tmp = digit[i];
+            func(num + 1);
+            isused[i] = 0;
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+        cin >> digit[i];
+    sort(digit, digit + N);
+    func(0);
+    return 0;
+}
