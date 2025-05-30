3e78fd48e8b93db1d8faad995a2f4711f408abe0
kyucho1009 <chocastle4@gmail.com>
2025-05-30 15:23:56 +0900

    BOJ15552

---
+++ BOJ15552.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int T;
+    int A, B;
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        cin >> A >> B;
+        cout << A + B << "\n";
+    }
+    return 0;
+}
