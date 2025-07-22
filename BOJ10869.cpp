5656f930b19699200d89a926fbdd66b176d731c4
kyucho1009 <chocastle4@gmail.com>
2025-07-22 15:17:09 +0900

    Create: BOJ10869.cpp

---
+++ BOJ10869.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int A;
+int B;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> A >> B;
+    cout << A + B << "\n";
+    cout << A - B << "\n";
+    cout << A * B << "\n";
+    cout << A / B << "\n";
+    cout << A % B << "\n";
+    return 0;
+}
