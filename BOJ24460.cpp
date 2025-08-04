23e4790f054a6ef8e4fae407c2d9800f2252a29c
kyucho1009 <chocastle4@gmail.com>
2025-08-04 15:19:48 +0900

    Style: 주석작성

--- BOJ24460.cpp
+++ BOJ24460.cpp
@@ -8,9 +8,9 @@ int func(int x, int y, int n)
 {
     int arr[4];
     vector<int> V;
-    if (n == 1)
+    if (n == 1) // 해당 조건이 없으면 N이 1일때 무한루프
         return board[0][0];
-    if (n == 2)
+    if (n == 2) // N이 2일때 2번쨰로 작은 수 반환
     {
         for (int i = x; i < x + n; i++)
             for (int j = y; j < y + n; j++)
@@ -18,14 +18,14 @@ int func(int x, int y, int n)
         sort(V.begin(), V.end());
         return V[1];
     }
-    else
+    else // N이 2가 될때까지 자르기
     {
         arr[0] = func(x, y, n / 2);
         arr[1] = func(x + n / 2, y, n / 2);
         arr[2] = func(x, y + n / 2, n / 2);
         arr[3] = func(x + n / 2, y + n / 2, n / 2);
         sort(arr, arr + 4);
-        return arr[1];
+        return arr[1]; // 반환 받은 수들 중 2번째로 작은 수 반환
     }
 }
 
