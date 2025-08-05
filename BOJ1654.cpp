564bb1cc4bef003e5769c323c03ee2fffd678292
kyucho1009 <chocastle4@gmail.com>
2025-08-05 19:12:12 +0900

    Modify: using parametric serach

--- BOJ1654.cpp
+++ BOJ1654.cpp
@@ -3,42 +3,43 @@
 #include <vector>
 using namespace std;
 
+int K;
+int N;
+int arr[10001];
+
+bool solve(int len)
+{
+    long long ans;
+    ans = 0;
+    for (int i = 0; i < K; i++)
+        ans += arr[i] / len;
+    return ans >= N;
+}
+
 int main()
 {
-    int after;
-    int before;
-    long long ans;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> K >> N;
     long long sum;
-    long long low;
-    long long mid;
-    long long high;
-    vector<int> len(10000);
-
-    ans = 0;
-    sum = 0;                         // sum 초기화
-    cin >> before >> after;          // 조건 입력
-    for (int i = 0; i < before; i++) // 테스트 케이스 입력
+    sum = 0;
+    for (int i = 0; i < K; i++)
     {
-        cin >> len[i];
-        sum += len[i]; // 모든 랜 길이의 합
+        cin >> arr[i];
+        sum += arr[i];
     }
-    low = 1;            // 최소값 설정
-    high = sum / after; // 최댓갑 설정
-    mid = (low + high) / 2;
-    while (low <= high)
+    long long st;
+    long long en;
+    st = 1;
+    en = sum / N;
+    while (st < en)
     {
-        sum = 0;
-        mid = (low + high) / 2;
-        for (int i = 0; i < before; i++)
-            sum += len[i] / mid;
-        if (sum >= after)
-        {
-            low = mid + 1;
-            ans = max(ans, mid);
-        }
+        int mid;
+        mid = (st + en + 1) / 2;
+        if (solve(mid))
+            st = mid;
         else
-            high = mid - 1;
+            en = mid - 1;
     }
-    cout << ans;
-    return 0;
+    cout << st;
 }
