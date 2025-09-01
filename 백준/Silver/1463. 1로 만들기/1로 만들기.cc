c5f042e1924be0c87c40961a74f2d41fcc048b29
cho kyu hyeong <126551132+kyucho1009@users.noreply.github.com>
2025-09-01 20:58:05 +0900

    [Silver III] Title: 1로 만들기, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int dp[1000001];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i = 1; i < N; i++)
+    {
+        if (i * 2 < 1000001)
+        {
+            if (dp[i * 2] != 0)
+                dp[i * 2] = min(dp[i * 2], dp[i] + 1);
+            else
+                dp[i * 2] = dp[i] + 1;
+        }
+        if (i * 3 < 1000001)
+        {
+            if (dp[i * 3] != 0)
+                dp[i * 3] = min(dp[i * 3], dp[i] + 1);
+            else
+                dp[i * 3] = dp[i] + 1;
+        }
+        if (i + 1 < 1000001)
+        {
+            if (dp[i + 1] != 0)
+                dp[i + 1] = min(dp[i + 1], dp[i] + 1);
+            else
+                dp[i + 1] = dp[i] + 1;
+        }
+    }
+    cout << dp[N];
+    return 0;
+}
