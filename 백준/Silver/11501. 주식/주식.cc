d2ff922d637b516bc0eaa5ce8ed8833cac675748
cho kyu hyeong <126551132+kyucho1009@users.noreply.github.com>
2026-01-28 01:44:42 +0900

    [Silver II] Title: 주식, Time: 560 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/11501. 주식/주식.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int T;
+int N;
+int temp[1000001];
+int price[1000001];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> T;
+    while (T--)
+    {
+        long long ans;
+        ans = 0;
+        cin >> N;
+        for (int i = 0; i < N; i++)
+            cin >> price[i];
+        int mx = 0;
+        for (int i = N - 1; i >= 0; i--)
+        {
+            mx = max(mx, price[i]);
+            temp[i] = mx;
+        }
+        for (int i = 0; i < N; i++)
+            ans += temp[i] - price[i];
+        cout << ans << "\n";
+    }
+    return 0;
+}
